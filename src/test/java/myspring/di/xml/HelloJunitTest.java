fc16a4ba35ce3deb7521abf7885fc99a85f1623b
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-04-27 17:45:15 +0900

    junit case

---
+++ src/test/java/myspring/di/xml/HelloJunitTest.java
@@ -0,0 +1,25 @@
+package myspring.di.xml;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+//Assertions에 포함된 모든 static method를 import 한다.
+import static org.junit.jupiter.api.Assertions.*; // static import
+
+public class HelloJunitTest {
+	
+	@Test
+	void helloBean() {
+		// 1. Container 객체 생성
+		ApplicationContext context = new GenericXmlApplicationContext("classpath:hello-di.xml"); // XML 파일을 알려줘야 알아서 객체 생성을 함
+		// 2. Container 객체가 생성한 Spring Bean 요청하기
+		Hello helloById = (Hello)context.getBean("hello");
+		Hello helloByType = context.getBean("hello", Hello.class);
+		
+		// Spring Bean의 주소 비교
+		System.out.println(helloById == helloByType);
+		assertSame(helloById, helloByType);
+	}
+}
