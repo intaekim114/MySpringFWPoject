d7ef801d424baff1dbce05350c1e387b17a82f89
Intae Kim <121932300+intaekim114@users.noreply.github.com>
2025-04-28 11:19:43 +0900

    spring test case update

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -3,10 +3,13 @@ package myspring.di.xml;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import javax.annotation.Resource;
 
 // SpringExtension 클래스는 ApplicationContext(컨테이너) 객체를 생성하는 역할을 함
 @ExtendWith(SpringExtension.class)
@@ -15,9 +18,29 @@ public class HelloSpringTest {
 	@Autowired
 	Hello hello; // getBean("hello")
 	
+	@Autowired
+	Printer strPrinter; // bean의 id값을 넣어주자
+	
+	@Autowired // 타입을 기준으로 bean을 찾는 것  
+	@Qualifier("strPrinter") // id이름과 다르게 객체를 선언하고 싶을 때
+	Printer printer;
+	
+	@Resource(name = "hello") // 스프링 어노테이션이 아닌 자바쪽 어노테이션
+	Hello helloBean;
+	
+	
 	@Test
 	void helloSpringBean() {
 		assertEquals("Hello 스프링", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 스프링", strPrinter.toString());
+		
+		System.out.println(strPrinter.getClass().getName());
+		
+		assertEquals("Hello 스프링", printer.toString());
+		
+		assertEquals("Hello 스프링", helloBean.sayHello());
+		
 		
 	}
 }
